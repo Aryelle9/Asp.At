b05099a666c11809632a1c897b61eb76e28ed37f
Aryelle9 <maria.a.santos9@aluno.senai.br>
2026-05-06 15:24:51 -0300

    Atividade1

---
+++ AT1/AT1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
